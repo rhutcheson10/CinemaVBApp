b6180a92848b556ba288d24516fe62cf7eeb4b53
rhutcheson10 <41714048+rhutcheson10@users.noreply.github.com>
2019-08-20 14:36:04 +1200

    Add project files.

---
+++ CinemaVBApp/Form1.vb
@@ -0,0 +1,148 @@
+﻿Public Class Form
+    Public Function Adult_Before_5(quantity As Integer, person As String, day As Boolean, time As Integer) As Double
+        Dim ticket_price = 14.5
+        Dim total_price = ticket_price * quantity
+
+        If day = False Then 'False means that it does not fall on a tuesday 
+
+            If quantity > 0 And quantity <= 10 Then
+
+                If person = "Adult" Then
+                    If time < 5 Then
+                        Return total_price
+                    End If
+                End If
+
+            End If
+
+        End If
+
+    End Function
+
+    Public Function Adult_After_5(quantity As Integer, person As String, day As String, time As Integer) As Double
+        Dim ticket_price = 17.5
+        Dim total_price = ticket_price * quantity
+
+        If day = False Then 'False means that it does not fall on a tuesday 
+
+            If quantity > 0 And quantity <= 10 Then
+
+                If person = "Adult" Then
+                    If time > 5 Then
+                        Return total_price
+                    End If
+                End If
+
+            End If
+
+        End If
+
+    End Function
+    Public Function Adult_Tuesday(quantity As Integer, person As String, day As String) As Int32
+        Dim ticket_price = 13
+        Dim total_price = ticket_price * quantity
+
+        If day = True Then 'True means that it falls on a tuesday 
+
+            If quantity > 0 And quantity <= 10 Then
+
+                If person = "Adult" Then
+                    Return total_price
+                End If
+
+            End If
+
+        End If
+
+    End Function
+
+    Public Function Child_Under_16(quantity As Integer, person As String) As Int32
+        Dim ticket_price = 12
+        Dim total_price = ticket_price * quantity
+
+        If quantity > 0 And quantity <= 10 Then
+
+            If person = "Child" Then
+                Return total_price
+            End If
+
+        End If
+    End Function
+
+    Public Function Senior(quantity As Integer, person As String) As Double
+        Dim ticket_price = 12.5
+        Dim total_price = ticket_price * quantity
+
+        If quantity > 0 And quantity <= 10 Then
+
+            If person = "Senior" Then
+                Return total_price
+            End If
+
+        End If
+    End Function
+    Public Function Student(quantity As Integer, person As String, day As Boolean) As Int32
+        Dim ticket_price = 14
+        Dim total_price = ticket_price * quantity
+
+        If day = False Then 'False means that it does not fall on a tuesday 
+            If quantity > 0 And quantity <= 10 Then
+
+                If person = "Student" Then
+                    Return total_price
+                End If
+            End If
+
+        End If
+    End Function
+    Public Function Family_Pass(quantity) As Integer
+        Dim ticket_price = 46
+        Dim total_price = ticket_price * quantity
+
+
+        If quantity > 0 And quantity <= 10 Then
+
+
+            Return total_price
+        End If
+
+
+    End Function
+
+    Public Function Chick_Flick_Thursday(quantity As Integer, person As String) As Double
+        Dim ticket_price = 21.5
+        Dim total_price = ticket_price * quantity
+
+        If quantity > 0 And quantity <= 10 Then
+
+            If person = "Adult" Then
+                Return total_price
+            End If
+
+        End If
+
+    End Function
+    Public Function Kids_Carers(quantity As Integer, day As Boolean, holiday As Boolean) As Integer
+        Dim ticket_price = 12
+        Dim total_price = ticket_price * quantity
+
+        If day = True Then 'True means that it falls on Wednesday
+            If holiday = False Then 'Not a holiday
+
+
+                If quantity > 0 And quantity <= 10 Then
+
+                    Return total_price
+
+                End If
+            End If
+
+        End If
+    End Function
+
+
+
+    Private Sub Cinemas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ UnitTestProject1/UnitTest1.vb
@@ -0,0 +1,88 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports CinemaVBApp
+
+<TestClass()> Public Class UnitTest1
+
+    <TestMethod()> Public Sub Adult_Before_5_Test()
+
+
+        Dim form1 As Form = New Form
+        Dim adult_before_5 = form1.Adult_Before_5(2, "Adult", False, 4)
+
+
+        Assert.AreEqual(29.0, adult_before_5)
+
+
+
+    End Sub
+
+    <TestMethod()> Public Sub Adult_After_5_Test()
+
+
+        Dim form1 As Form = New Form
+        Dim adult_after_5 = form1.Adult_After_5(1, "Adult", False, 6)
+
+        Assert.AreEqual(17.5, adult_after_5)
+    End Sub
+
+
+    <TestMethod()> Public Sub Adult_Tuesday()
+
+
+        Dim form1 As Form = New Form
+        Dim adult_tuesday = form1.Adult_Tuesday(1, "Adult", True)
+
+        Assert.AreEqual(13, adult_tuesday)
+    End Sub
+
+    <TestMethod()> Public Sub Child_Under_16()
+
+
+        Dim form1 As Form = New Form
+        Dim child_under_16 = form1.Child_Under_16(1, "Child")
+        Assert.AreEqual(12, child_under_16)
+
+    End Sub
+
+    <TestMethod()> Public Sub Senior()
+
+        Dim form1 As Form = New Form
+        Dim senior = form1.Senior(1, "Senior")
+        Assert.AreEqual(12.5, senior)
+
+    End Sub
+
+    <TestMethod()> Public Sub Student()
+
+        Dim form1 As Form = New Form
+        Dim student = form1.Student(1, "Student", False)
+        Assert.AreEqual(14, student)
+
+    End Sub
+
+    <TestMethod()> Public Sub Chick_Flick()
+
+        Dim form1 As Form = New Form
+        Dim chick_flick = form1.Chick_Flick_Thursday(1, "Adult")
+        Assert.AreEqual(21.5, chick_flick)
+
+    End Sub
+
+    <TestMethod()> Public Sub Kids_Carers()
+
+        Dim form1 As Form = New Form
+        Dim kids_carers = form1.Kids_Carers(1, True, False)
+        Assert.AreEqual(12, kids_carers)
+
+    End Sub
+
+    <TestMethod()> Public Sub Family_Pass()
+
+        Dim form1 As Form = New Form
+        Dim family_pass = form1.Family_Pass(1)
+        Assert.AreEqual(46, family_pass)
+
+    End Sub
+
+End Class
